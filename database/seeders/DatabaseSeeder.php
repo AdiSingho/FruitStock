69396854a72fb5e8ab9e124175ccf72d3db736af
Singgih Adi Nugroho <dwiudin508@gmail.com>
2026-07-24 06:33:28 +0700

    feat: membuat seeder untuk data master, buah, dan simulasi stok FEFO

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,17 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+            $this->call([
+        MasterDataSeeder::class,
+        ]);
+        
+        $this->call([
+            MasterDataSeeder::class,
+            BuahSeeder::class,
+            StokSeeder::class,
+        ]);
+        
     }
+    
 }
